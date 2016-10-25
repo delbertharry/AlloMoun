dbeec513b953e29d30a8838330db316deb7dfabc
delbert <delbertharry@gmail.com>
2016-10-25 06:36:11 -0400

    Working sample.  Draft1

--- AlloMoun/Resources/Resource.Designer.cs
+++ AlloMoun/Resources/Resource.Designer.cs
@@ -63,6 +63,36 @@ namespace AlloMoun
 			// aapt resource value: 0x7f050000
 			public const int MyButton = 2131034112;
 			
+			// aapt resource value: 0x7f050001
+			public const int button1 = 2131034113;
+			
+			// aapt resource value: 0x7f05000a
+			public const int button10 = 2131034122;
+			
+			// aapt resource value: 0x7f050002
+			public const int button2 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int button3 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int button4 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int button5 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int button6 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int button7 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int button8 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int button9 = 2131034121;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
